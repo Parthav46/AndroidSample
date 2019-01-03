bb7b1bdefe7d2518f97b6220a12929e8b3a6f25e
Parthav46 <parthav4698>
2019-01-04 01:08:47 +0530

    multiple counter handle

--- app/src/main/java/com/example/footballref/MainActivity.java
+++ app/src/main/java/com/example/footballref/MainActivity.java
@@ -2,24 +2,31 @@ package com.example.footballref;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    int score;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        score = 0;
     }
 
     public void increaseGoal(View view) {
-        TextView counter = (TextView) findViewById(R.id.counter);
+        TextView counter = (TextView) findViewById(view.getId());
+        int score = Integer.parseInt(counter.getText().toString());
         score++;
         String counterValue = String.valueOf(score);
         counter.setText(counterValue);
+    }
+
+    public void resetGoal(View view) {
+        TextView counterA = (TextView) findViewById(R.id.counterA);
+        TextView counterB = (TextView) findViewById(R.id.counterB);
+        String counterValue = "0";
+        counterA.setText(counterValue);
+        counterB.setText(counterValue);
     }
 }
