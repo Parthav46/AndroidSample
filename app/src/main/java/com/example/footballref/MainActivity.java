7f7a8c5b4259016dd71e49fd9691d216ec0ae0e1
Parthav46 <parthav4698>
2019-01-04 00:40:21 +0530

    counter created

--- app/src/main/java/com/example/footballref/MainActivity.java
+++ app/src/main/java/com/example/footballref/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.footballref;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        score = 0;
+    }
+
+    public void increaseGoal(View view) {
+        TextView counter = (TextView) findViewById(R.id.counter);
+        score++;
+        String counterValue = String.valueOf(score);
+        counter.setText(counterValue);
     }
 }
