043739d1579b330deacb175f94778c68d4102ab7
Parthav46 <parthav4698>
2019-01-04 23:01:38 +0530

    timer added

--- app/src/main/java/com/example/footballref/MainActivity.java
+++ app/src/main/java/com/example/footballref/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.footballref;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,6 +10,14 @@ public class MainActivity extends AppCompatActivity {
 
     private int scoreA;
     private int scoreB;
+    private long time;
+    private boolean isTimerRunning;
+
+    private TextView counterA;
+    private TextView counterB;
+    private TextView timeView;
+
+    private CountDownTimer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
 
         scoreA = 0;
         scoreB = 0;
+        time = 120;
+        isTimerRunning = false;
 
-        final TextView counterA = (TextView) findViewById(R.id.counterA);
-        final TextView counterB = (TextView) findViewById(R.id.counterB);
+        counterA = (TextView) findViewById(R.id.counterA);
+        counterB = (TextView) findViewById(R.id.counterB);
+        timeView = (TextView) findViewById(R.id.time);
 
         counterA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,11 +70,49 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        timeView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isTimerRunning){
+                    timer.cancel();
+                    isTimerRunning = false;
+                    String timeValue = timeView.getText().toString();
+                    int dividerIndex = timeValue.indexOf(':');
+                    int minute = Integer.parseInt(timeValue.substring(0, dividerIndex));
+                    int second = Integer.parseInt(timeValue.substring(dividerIndex+1, timeValue.length()));
+                    time = (long)((minute * 60) + second);
+                } else {
+                    timer = getTimer(time);
+                    timer.start();
+                }
+            }
+        });
+    }
+
+    CountDownTimer getTimer(long timePeriod) {
+        return new CountDownTimer(timePeriod * 1000L, 10) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                isTimerRunning = true;
+                long timeValue = millisUntilFinished / 1000L;
+                String timeString = String.valueOf(timeValue / 60L) + ":" + String.valueOf(timeValue % 60L);
+                timeView.setText(timeString);
+            }
+
+            @Override
+            public void onFinish() {
+                isTimerRunning = false;
+            }
+        };
     }
 
     public void resetGoal(View view) {
-        TextView counterA = (TextView) findViewById(R.id.counterA);
-        TextView counterB = (TextView) findViewById(R.id.counterB);
+        if(isTimerRunning){
+            timer.cancel();
+            isTimerRunning = false;
+        }
+        time = 120;
         scoreA = 0;
         scoreB = 0;
         String counterValue = "0";
