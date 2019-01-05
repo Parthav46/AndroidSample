929d15ef2b1af2dd482db787f9d53ce06311d55f
Parthav46 <parthav4698>
2019-01-05 15:20:47 +0530

    porgress bar added

--- app/src/main/java/com/example/footballref/MainActivity.java
+++ app/src/main/java/com/example/footballref/MainActivity.java
@@ -4,18 +4,21 @@ import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private int scoreA;
     private int scoreB;
-    private long time;
+    private long time = 120000L;
+    private long timeLeft;
     private boolean isTimerRunning;
 
     private TextView counterA;
     private TextView counterB;
     private TextView timeView;
+    private ProgressBar timeProgress;
 
     private CountDownTimer timer;
 
@@ -26,12 +29,13 @@ public class MainActivity extends AppCompatActivity {
 
         scoreA = 0;
         scoreB = 0;
-        time = 120;
+        timeLeft = time;
         isTimerRunning = false;
 
         counterA = (TextView) findViewById(R.id.counterA);
         counterB = (TextView) findViewById(R.id.counterB);
         timeView = (TextView) findViewById(R.id.time);
+        timeProgress = (ProgressBar) findViewById(R.id.time_progress);
 
         counterA.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,35 +80,57 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(isTimerRunning){
                     timer.cancel();
+                    deactivateCounter();
                     isTimerRunning = false;
                     String timeValue = timeView.getText().toString();
                     int dividerIndex = timeValue.indexOf(':');
                     int minute = Integer.parseInt(timeValue.substring(0, dividerIndex));
                     int second = Integer.parseInt(timeValue.substring(dividerIndex+1, timeValue.length()));
-                    time = (long)((minute * 60) + second);
+                    timeLeft = (long)((minute * 60) + second) * 1000;
                 } else {
-                    timer = getTimer(time);
+                    timer = getTimer(timeLeft);
+                    activateCounter();
                     timer.start();
                 }
             }
         });
+
+        setTime(timeLeft);
+        deactivateCounter();
     }
 
-    CountDownTimer getTimer(long timePeriod) {
-        return new CountDownTimer(timePeriod * 1000L, 10) {
+    private CountDownTimer getTimer(long timePeriod) {
+        return new CountDownTimer(timePeriod, 10) {
             @Override
             public void onTick(long millisUntilFinished) {
                 isTimerRunning = true;
-                long timeValue = millisUntilFinished / 1000L;
-                String timeString = String.valueOf(timeValue / 60L) + ":" + String.valueOf(timeValue % 60L);
-                timeView.setText(timeString);
+                setTime(millisUntilFinished);
             }
 
             @Override
             public void onFinish() {
                 isTimerRunning = false;
+                timeLeft = 0;
+                setTime(timeLeft);
             }
         };
+    }
+
+    private void activateCounter() {
+        counterA.setEnabled(true);
+        counterB.setEnabled(true);
+    }
+
+    private void deactivateCounter() {
+        counterA.setEnabled(false);
+        counterB.setEnabled(false);
+    }
+
+    private void setTime(long timePeriod) {
+        String seconds = String.valueOf((timePeriod / 1000L) % 60L);
+        String timeString = String.valueOf(timePeriod / 60000L) + ":" + ((seconds.length() == 1) ? "0" : "") + seconds;
+        timeView.setText(timeString);
+        timeProgress.setProgress((int)(timePeriod * 10000L / time) );
     }
 
     public void resetGoal(View view) {
@@ -112,7 +138,9 @@ public class MainActivity extends AppCompatActivity {
             timer.cancel();
             isTimerRunning = false;
         }
-        time = 120;
+        deactivateCounter();
+        timeLeft = time;
+        setTime(timeLeft);
         scoreA = 0;
         scoreB = 0;
         String counterValue = "0";
